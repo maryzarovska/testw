cd2eb5c727ea03c4c5574423a8977fe18360600a
maryzarovska <maryzarovska@gmail.com>
2024-08-31 17:05:30 +0200

    error message

--- client/src/Register.tsx
+++ client/src/Register.tsx
@@ -6,12 +6,18 @@ function Register() {
     const [email, setEmail] = React.useState("");
     const [username, setUsername] = React.useState("");
     const [password, setPassword] = React.useState("");
+    const [errorMessage, setErrorMessage] = React.useState("");
 
     function register(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         axios.post("/api/users/signup", {
             email, username, password
-        }).then(response => console.log(response.data))
+        }).then(response => {
+            console.log(response.data);
+            if(!response.data.success) {
+                setErrorMessage(response.data.message);
+            }
+        })
     }
 
     return (
@@ -22,6 +28,9 @@ function Register() {
                 <input type="text" value={email} placeholder='Email' onChange={event => setEmail(event.target.value)} /> <br />
                 <input type="text" value={username} placeholder='Username' onChange={event => setUsername(event.target.value)} /> <br />
                 <input type="password" value={password} placeholder='Password' onChange={event => setPassword(event.target.value)} /> <br /><br />
+                {errorMessage?
+                <p>{errorMessage}</p>:
+                <></>}
                 <button type="submit">Register</button>
             </form>
         </>
