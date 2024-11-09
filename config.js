3c64046813543c79ae99229b50e3c6c8360dd09c
andriipopovgit <andrijpopow07@gmail.com>
2024-11-09 18:21:02 +0200

    fix .gitignore

--- config.js
+++ config.js
@@ -2,14 +2,8 @@ const config = {
     db: {
         host: "localhost",
         user: "root",
-        password: "Mdls22113123",
-        // password: "admin",
+        password: "admin",
         database: "funtest",
-        
-        // host: "sql11.freemysqlhosting.net",
-        // user: "sql11650227",
-        // password: "MSjiJF5d9Y",
-        // database: "sql11650227",
     },
     listPerPage: 10,
     jwtSecret: 'TOP_SECRET',
